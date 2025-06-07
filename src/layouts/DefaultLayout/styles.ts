1e70198a46e8282a98831af2bafda70190904b4c
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-06-07 19:42:12 -0300

    Update

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
-    /* max-width: 74rem; */
-    /* padding: 2rem 0; */
-    background: ${({theme}) => theme.background};
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+    //background: ${({theme}) => theme.background};
+    background: deeppink;
 `;
