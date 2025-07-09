a554db709fb5ccf06d5a151198315a8b80e117de
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-07-09 03:44:39 -0300

    Create button variants

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,63 +1,47 @@
 import styled, {css} from "styled-components";
 import { mixins } from "../../styles/mixins";
 
-export type ButtonVariant = "primary" | "secondary" | "icon";
 
-interface ButtonContainerProps{
-    variant: ButtonVariant;
-}
-
-const buttonVariants = {
-    primary: "yellow",
-    primary_hover: "yellow-dark",
-    secondary: "base-button",
-    secondary_hover: "base-hover",
-    icon: "purple-dark",
-    icon_hover: "purple"
-} as const;
-
-export const ButtonContainer = styled.div<ButtonContainerProps>`
+const BaseButton = styled.button`
+    border: none;
     border-radius: 6px;
-
-    background: ${props => props.theme[buttonVariants[props.variant]]};
-    width: ${props => {
-        if(props.variant === "primary")
-            return "100%";
-    }};
-
-    padding: ${props => props.variant === "primary" ? ".75rem": "1rem"};
-    color: white;
-    ${
-        props => {
-            switch (props.variant) {
-                case "primary":
-                    return mixins.button_g;
-                case "secondary":
-                    return mixins.button_m;
-            
-                default:
-                    break;
-            }
-        }
-    }
-
     text-align: center;
+`;
 
-    ${props => {
-        if(props.variant === "secondary" ){
-            return css`
-                text-transform: uppercase;
-                display: flex;
-                align-items: "center";
-                gap: .0938rem;
-                padding: .5rem;
-                width: fit-content;
-                color: ${props.theme["base-text"]};
-                `
-        }
-        }
-    };
+export const PrimaryButton = styled(BaseButton)`
+    width: 100%;
+    background: ${props => props.theme.yellow};
+    padding: .75rem;
+    color: white;
+    ${mixins.button_g};
+
     &:hover{
-        background: ${props => props.variant === "primary" ? props.theme[buttonVariants.primary_hover] : "none"};
+        background: ${props => props.theme["yellow-dark"]};
+    }
+`;
+
+export const SecondaryButton = styled(BaseButton)`
+    ${mixins.button_m};
+    text-transform: uppercase;
+    display: flex;
+    align-items: "center";
+    gap: .0938rem;
+    padding: .5rem;
+    width: fit-content;
+    color: ${props => props.theme["base-text"]};
+
+    &:hover{
+        background: ${props => props.theme["base-hover"]};
+    }
+`;
+
+export const IconButton = styled(BaseButton)`
+    padding: .5rem;
+    display: flex;
+    color: ${props => props.theme["white"]};
+    background: ${props => props.theme["purple-dark"]};
+
+    &:hover{
+        background: ${props => props.theme["purple"]};
     }
 `;
