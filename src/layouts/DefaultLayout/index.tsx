088a75a0e5a0b739e52f20402b7aada5011909f2
Sabrina Meireles Coelho Teodoro <sabrina.mct@xcmg-america.local>
2025-07-11 17:33:05 -0300

    Test Default Layout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,17 +1,14 @@
 import { Header } from "../../components/Header";
-import { CatalogItem } from "../../pages/Home/components/Catalog/CatalogItem";
 import { LayoutContainer } from "./styles";
+import { Catalog } from "../../pages/Home/components/Catalog";
+import { Hero } from "../../pages/Home/components/Hero";
+
 export function DefaultLayout(){
     return(
         <LayoutContainer>
             <Header/>
-            <CatalogItem 
-                description="Uma dose de café expresso com o dobro de leite e espuma cremosa"
-                name="Latte"
-                tags={["Tradicional", "com leite"]}
-                price={10.50}
-                img=""
-            />
+            <Hero />
+            <Catalog />
         </LayoutContainer>
     );
 }
