e56f946691d9a43c87ecc801ebcfb6f04e6e43d9
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-07-14 01:40:49 -0300

    Fix

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const HomeContainer = styled.div`
     background: ${({theme}) => theme.background};
-    max-width: 70rem;
     margin: auto;
     display: flex;
     flex-direction: column;
