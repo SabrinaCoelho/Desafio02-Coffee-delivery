7e2867e1c7d405c2d2b6b5acdfc06e01a3a73ac8
Sabrina Meireles Coelho Teodoro <sabrina.mct@xcmg-america.local>
2025-07-15 17:26:35 -0300

    Add /checkout

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Home } from "./pages/Home";
+import { Checkout } from "./pages/Checkout";
 
 export function Router(){
     return (
@@ -8,6 +9,7 @@ export function Router(){
             <Route path="/" element={<DefaultLayout />}>
                 <Route path="/" element={<Home />}/>
                 <Route path="/hello" element={<h1>HELLO WARUDO!</h1>}/>
+                <Route path="/checkout" element={<Checkout />}/>
             </Route>
         </Routes>
     );
