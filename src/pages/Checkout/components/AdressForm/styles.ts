c1d3a9ccf95502f46489a0b38f1d51b561937417
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-08-11 00:10:46 -0300

    Add message error comp

--- src/pages/Checkout/components/AdressForm/styles.ts
+++ src/pages/Checkout/components/AdressForm/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mixins } from "../../../../styles/mixins";
 
 export const FormContainer = styled.div`
     width: 100%;
@@ -10,4 +11,9 @@ export const FormContainer = styled.div`
 export const InlineForm = styled.div`
     display: flex;
     gap: .75rem;
+`;
+
+export const MessageError = styled.small`
+    color: red;
+    ${mixins.text_xs}
 `;
