4e70d8b01db0082cbae2c181bffc89d306828f05
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-08-11 00:06:54 -0300

    Add react-router-dom Link comp. to test nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { CityLocation, HeaderContainer } from "./styles";
 import logo from "../../assets/Logo.svg";
 import { MapPinIcon, ShoppingCartIcon } from "@phosphor-icons/react";
 import { Cart } from "../Cart";
+import { Link } from "react-router";
 
 export function Header (){
     return(
@@ -14,9 +15,11 @@ export function Header (){
                     <MapPinIcon size={22} weight="fill" />
                     Porto Alegre, RS
                 </CityLocation>
-                <Cart>
-                    <ShoppingCartIcon size={22} weight="fill" />
-                </Cart>
+                <Link to="/checkout">
+                    <Cart>
+                        <ShoppingCartIcon size={22} weight="fill" />
+                    </Cart>
+                </Link>
             </div>
         </HeaderContainer>
     );
