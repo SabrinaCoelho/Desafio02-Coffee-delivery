e416a70c66fbff87ea2681d6fafe441d7f823ed2
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-08-11 00:07:48 -0300

    Add MessageError comp to label

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -1,16 +1,21 @@
+import { MessageError } from "../../pages/Checkout/components/AdressForm/styles";
 import { InputTextContainer, Text } from "./styles";
 import type { InputHTMLAttributes } from "react";
 
 interface InputTextProps extends InputHTMLAttributes<HTMLInputElement> {
     optional?: boolean;
     width?: "full" | "12.5rem";
+    errorMessage?: string;
 }
 
 export function InputText({optional = false, width = "full", ...props}: InputTextProps) {
     return(
-        <InputTextContainer width={width}>
-            <Text type="text" placeholder={props.placeholder} {...props}/>
-            {optional && (<span>Opcional</span>)}
-        </InputTextContainer>
+        <label>
+            <InputTextContainer width={width}>
+                <Text type="text" placeholder={props.placeholder} {...props}/>
+                {optional && (<span>Opcional</span>)}
+            </InputTextContainer>
+            <MessageError>{props.errorMessage}</MessageError>
+        </label>
     );
 }
