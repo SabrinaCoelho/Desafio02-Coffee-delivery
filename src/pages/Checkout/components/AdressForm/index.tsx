435385989122c6f24e914ddf565429be17e09999
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-08-11 00:09:53 -0300

    Add message error support

--- src/pages/Checkout/components/AdressForm/index.tsx
+++ src/pages/Checkout/components/AdressForm/index.tsx
@@ -9,7 +9,7 @@ import { useContext } from "react";
 export function AdressForm(){
     
     const {order} = useContext(CartContext);
-    const { register } = useFormContext();
+    const { register, formState: { errors } } = useFormContext();
     return(
         <FormContainer>
             <InputText 
@@ -17,11 +17,13 @@ export function AdressForm(){
                 placeholder="CEP" 
                 width="12.5rem"
                 {...register("zip")}
+                errorMessage={errors && errors.zip && `${errors.zip?.message}`}
             />
             <InputText 
                 id="street"
                 placeholder="Rua" 
                 {...register("street")}
+                errorMessage={errors && errors.street && `${errors.street?.message}`}
             />
             <InlineForm>
                 <InputText 
@@ -29,12 +31,14 @@ export function AdressForm(){
                     placeholder="Número" 
                     width="12.5rem"
                     {...register("number")}
+                    errorMessage={errors && errors.number && `${errors.number?.message}`}
                 />
                 <InputText 
                     id="comple"
                     placeholder="Complemento" 
                     optional={true} 
                     {...register("comple")}
+                    errorMessage={errors && errors.comple && `${errors.comple?.message}`}
                 />
             </InlineForm>
             <InlineForm>
@@ -43,16 +47,19 @@ export function AdressForm(){
                     placeholder="Bairro" 
                     width="12.5rem" 
                     {...register("neighborhood")}
+                    errorMessage={errors && errors.neighborhood && `${errors.neighborhood?.message}`}
                 />
                 <InputText 
                     id="city"
                     placeholder="Cidade" 
                     {...register("city")}
+                    errorMessage={errors && errors.city && `${errors.city?.message}`}
                 />
                 <InputText
                     id="state"
                     placeholder="UF" 
                     {...register("state")}
+                    errorMessage={errors && errors.state && `${errors.state?.message}`}
                 />
             </InlineForm>
         </FormContainer>
