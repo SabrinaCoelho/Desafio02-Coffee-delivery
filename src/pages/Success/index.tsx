c2813a759695c5425b8932c45c02f74f9bffc9f2
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-10-28 23:38:33 -0300

    Add Subtitle to Success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,11 +1,20 @@
 import { defaultTheme } from "../../styles/default";
-import { TitleL } from "../Home/components/Typography/styles";
-import { SuccessContainer } from "./styles";
+import { TextL_Regular, TitleL } from "../Home/components/Typography/styles";
+import { DeliveryRecap, SuccessContainer, SuccessImage, SuccessMessageTitle } from "./styles";
 
 export function Success(){
     return(
         <SuccessContainer>
-            <TitleL color={defaultTheme["yellow-dark"]}>Uhu! Pedido confirmado</TitleL>
+            <SuccessMessageTitle>
+                <TitleL color={defaultTheme["yellow-dark"]}>Uhu! Pedido confirmado</TitleL>
+                <TextL_Regular>Agora é só aguardar que logo o café chegará até você</TextL_Regular>
+            </SuccessMessageTitle>
+            <div>
+                <DeliveryRecap>
+                    Teste
+                </DeliveryRecap> 
+                <SuccessImage></SuccessImage>  
+            </div>
         </SuccessContainer>
     )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -8,11 +8,19 @@ export const SuccessContainer = styled.div`
 `;
 
 export const SuccessMessageTitle = styled.div`
-    
+    display: flex;
+    flex-direction: column;
 `;
 
 export const DeliveryRecap = styled.div`
-    
+    padding: 2.5rem;
+    border-width: 1px;
+    border-style: solid;
+    border-image: linear-gradient(to right, #DBAC2C, #8047F8) 1;
+    /* border-radius: 6px 44px 6px 44px; */
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
 `;
 
 export const SuccessImage = styled.div`
