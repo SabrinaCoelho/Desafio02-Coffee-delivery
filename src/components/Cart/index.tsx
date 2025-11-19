1986d9e605d8467c580639790b0dc4508d17bd57
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-11-18 22:03:31 -0300

    Qty of items in cart support

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,13 +1,25 @@
-import type { ReactNode } from "react";
-import { CartContainer } from "./styles";
+import { useContext, useMemo, type ReactNode } from "react";
+import { CartIconContainer, CartItemsQuantity} from "./styles";
+import { CartContext } from "../../contexts/CartContext";
 
 interface CartProps{
     children: ReactNode;
 }
 export function Cart({children}: CartProps){
+    const {order} = useContext(CartContext);
+
+    const itemsQuantity = useMemo(() => {
+         return order?.items?.reduce(
+            (acu, cur) => {
+                return acu + (cur.quantity ?? 0)
+            }, 0
+        ) ?? 0; 
+        }, [order]);
+
     return(
-        <CartContainer>
+        <CartIconContainer>
+            <CartItemsQuantity>{itemsQuantity}</CartItemsQuantity>
             {children}
-        </CartContainer>
+        </CartIconContainer>
     );
 }
--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -1,9 +1,24 @@
 import styled from "styled-components";
+import { mixins } from "../../styles/mixins";
 
-export const CartContainer = styled.div`
+export const CartIconContainer = styled.div`
     background: ${({theme}) => theme["yellow-light"]};
     border-radius: 6px;
     padding: .5rem;
     color: ${({theme}) => theme["yellow-dark"]};
     display: flex;
+    position: relative;
+`;
+
+export const CartItemsQuantity = styled.span`
+    background-color: ${props => props.theme["yellow-dark"]};
+    position: absolute;
+    color: ${props => props.theme["white"]};
+    ${mixins.text_xs}
+    min-width: 1.25rem;
+    text-align: center;
+    line-height: 1.375rem;
+    border-radius: 50%;
+    right: -10px;
+    top: -10px;
 `;
