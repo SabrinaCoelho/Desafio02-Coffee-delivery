5c73d03e34627ad893da776afc792d609e5ede20
Sabrina Teodoro <sabrinateodoro1019@hotmail.com>
2025-11-18 22:51:40 -0300

    Fix route basename

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <ThemeProvider theme={defaultTheme} >
       <GlobalStyle/>
-      <BrowserRouter>
+      <BrowserRouter basename="/Desafio02-Coffee-delivery/">
       <CartContextProvider>
         <Router/>
       </CartContextProvider>
